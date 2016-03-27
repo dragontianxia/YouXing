447017f29295f52031e9ad213867045f27c5da14
Xiaoya.Liu <lxy@wenba100.com>
2016-03-27 13:10:30 +0800

    Initial Commit

---
+++ YouXing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YouXing
+//
+//  Created by every2003 on 3/27/16.
+//  Copyright © 2016 wenba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
